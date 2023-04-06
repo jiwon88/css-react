1f2b2375abc2a441d553032995961984817540a7
Jiwon <119930659+jiwon88@users.noreply.github.com>
2023-04-06 18:17:51 +0900

    Add files via upload

---
+++ src/components/TailwindComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TailwindComponent() {
+    return(
+        <div>
+            <h1 className="text-3xl">안녕!</h1>
+            <button className="bg-blue-300 rounded-x1 px-2">Button</button>
+        </div>
+    );
+}
